8fa6b7a94d7487ce6b46b4699c540330b8c09492
DaWood32913 <woodleyjohnson@yahoo.com>
2020-03-04 11:10:45 -0500

    delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -186,7 +186,7 @@ router.put(
   ],
   async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
@@ -208,7 +208,7 @@ router.put(
       to,
       current,
       description
-    }
+    };
 
     try {
       const profile = await Profile.findOne({ user: req.user.id });
@@ -220,9 +220,32 @@ router.put(
       res.json(profile);
     } catch {
       console.error(err.message);
-      res.status(500).send('Server Error')
+      res.status(500).send('Server Error');
     }
   }
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch {
+    console.error(err.message);
+    res, status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
